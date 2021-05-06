841d8c12c2c1193b030afdb3e988ccacde5ce1b2
Your Name <dima.bader@optimumpartners.co>
2021-05-06 17:23:24 +0300

    update css file for mobile and desktop

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,36 @@
 
+
+window.addEventListener('DOMContentLoaded', (load) => {
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__in');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+let clickcount=0;
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
+    showMenu = false;
+  }
+}
+
+
 function getrandom() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/._-";
@@ -8,52 +40,69 @@ function getrandom() {
     return text;
 }
 
-
 function shorturl() {
 
     let intext=document.getElementById("text_sh");
-    if(intext.value.length==0){
+    if(intext.value.length !=0)
+    {
+        const div=document.getElementById("addch");
+        div.appendChild(adddiv(intext));
+    /*    if(clickcount!=0){
+            document.querySelector('.copy').addEventListener("click",coppy,false);
+        }*/
+}
+    else{
         intext.style.border="3px solid hsl(0, 87%, 67%)";
         let inpar=document.getElementById("nothing_in");
         inpar.innerHTML="please add a link";
         inpar.style.color="hsl(0, 87%, 67%)";
-        inpar.style.fontSize="20px"
-    }
-    else{
-    document.getElementById("addch").innerHTML="";
-    document.getElementById("addch").appendChild(adddiv());
-
+        inpar.style.fontSize="20px";
 }
 }
-function adddiv(){
-    let intext=document.getElementById("text_sh");
+function clear(intext){
     intext.style.border="none";
     let inpar=document.getElementById("nothing_in");
     inpar.innerHTML="";
-    var div = document.createElement('div');
-    var button = document.createElement('button');
-    var p=document.createElement('input');
-    var p2=document.createElement('span');
-    p2.innerText=intext.value;
-    p2.className="parg1";
-    div.appendChild(p2);
-    p.value="http://"+ getrandom();
-    p.style.border="none";
-    p.setAttribute("id","shlink");
-    p.className="parg2";
-    div.appendChild(p);
-  button.innerText="copy";
-    button.className="copy";
-    button.setAttribute("onclick","coppy()");
-    button.setAttribute("id","copyy");
-    div.appendChild(button);
-    div.className="addnew";
-    return div;
 }
-function coppy(){
-      /* Get the text field */
-  var copyText = document.getElementById("shlink");
-  var copybutton = document.getElementById("copyy");
+function adddiv(intext){
+    clear(intext);
+    clickcount +=1;
+    let makediv=document.createElement("div");
+    let makedivin=document.createElement("div");
+    makediv.className="addnew";
+    makedivin.className="parg1";
+    makedivin.innerHTML=intext.value;
+    let makeinput=document.createElement("input");
+    makeinput.className="parg2";
+    makeinput.innerHTML="http://"+`${getrandom()}`;
+    makeinput.setAttribute("id",`shlink${clickcount}`);
+    let makebutton=document.createElement("div");
+    makebutton.className="copy";
+    makebutton.setAttribute("id",`copyy${clickcount}`);
+    makebutton.setAttribute("onclick",function(){
+        coppy(`shlink${clickcount}`,`copyy${clickcount}`);});
+    makebutton.innerHTML="copy";
+    makediv.appendChild(makedivin);
+    makediv.appendChild(makeinput);
+    makediv.appendChild(makebutton);
+
+    return makediv;
+}
+  /*makediv += `<div class= addnew>
+
+    <div class="parg1">${intext.value}</div>
+    <div><input class="parg2" id="shlink${clickcount}" value="http://${getrandom()}"/></div>
+    <button class="copy" id="copyy${clickcount}" onclick="function(){
+        coppy("shlink${clickcount}","copyy${clickcount}");
+    }">copy</button></div>
+  </div>`;*/
+
+
+
+function coppy(idname, btnId){
+     /* Get the text field */
+  let copyText = document.getElementById(idname);
+  let copybutton = document.getElementById(btnId);
   /* Select the text field */
   copyText.select();
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
@@ -65,29 +114,6 @@ function coppy(){
   copybutton.innerText="copied!";
   copybutton.className="copied";
 }
+document.querySelector('.part-two__btn').addEventListener("click",shorturl);
 
-let menuBtn = document.querySelector('.menu-btn');
-let menuin = document.querySelector(".menu-btn__in");
-let nav = document.querySelector('.nav');
-let menuNav = document.querySelector('.menu-nav');
-let navItem =document.querySelector('.menu-nav__item');
-let showMenu = false;
-
-menuBtn.addEventListener('click',toggleMenu);
-
-function toggleMenu(){
-    if(!showMenu){
-        menuin.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItem.forEach(item => item.classList.add('open'));
-        showMenu =true;
-    }
-    else{
-        menuin.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItem.forEach(item => item.classList.remove('open'));
-        showMenu =false;
-    }
-}
+});
